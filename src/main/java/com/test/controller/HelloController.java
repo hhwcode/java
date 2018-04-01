1228c4a4ac85c29a9de40cb4f8b22e74ff349a67
hhwcode <1021457010@qq.com>
2018-04-01 21:25:54 +0800

    controller

--- src/main/java/com/test/controller/HelloController.java
+++ src/main/java/com/test/controller/HelloController.java
@@ -2,15 +2,16 @@ package com.test.controller;
 
 import com.test.model.User;
 import com.test.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 @Controller
 public class HelloController {
 
-    @Autowired
+    @Resource
     private UserService userService;
 
     @ResponseBody
